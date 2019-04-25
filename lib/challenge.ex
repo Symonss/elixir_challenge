cecfed7b4e34466428dc67b0695edb8969ddd97d
Midigo F <midifstar98@gmail.com>
2019-04-25 11:48:52 +0300

    Added function fetch

---
+++ lib/challenge.ex
@@ -0,0 +1,24 @@
+defmodule Challenge do
+  @moduledoc """
+  Documentation for Challenge.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Challenge.hello()
+      :world
+
+  """
+  def fetch do
+    HTTPoison.start
+    result = HTTPoison.get!("https://jsonplaceholder.typicode.com/todos/1")
+    JSON.decode(result.body) |> elem(1)    
+  end
+
+  def display do
+   
+  end  
+end
